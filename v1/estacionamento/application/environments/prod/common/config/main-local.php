ffc4b88fe51260301ff8be04fbf19492c4f5940b
LosLobos <igormorse@hotmail.com>
2017-06-07 02:33:31 -0300

    DB

--- v1/estacionamento/application/environments/prod/common/config/main-local.php
+++ v1/estacionamento/application/environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=mysql.hostinger.com.br;dbname=u866020361_uerj',
+            'username' => 'u866020361_uerj',
+            'password' => '2016.2',
             'charset' => 'utf8',
         ],
         'mailer' => [
